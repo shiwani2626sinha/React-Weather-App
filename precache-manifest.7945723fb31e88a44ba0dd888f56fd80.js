495c9e81c5ef440c5bb0ca7e841b86a832dc65d8
shiwani2626sinha <shiwani2626@gmail.com>
2020-07-18 23:55:26 +0530

    Updates

---
+++ precache-manifest.7945723fb31e88a44ba0dd888f56fd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3173f2cbb4cb2b14896b81deb5c3e2",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "da0ceff0c1673272a77a",
+    "url": "/React-Weather-App/static/css/main.7ba55fb0.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/React-Weather-App/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0ceff0c1673272a77a",
+    "url": "/React-Weather-App/static/js/main.43f6b727.chunk.js"
+  },
+  {
+    "revision": "f98aed09a5c088bbcd59",
+    "url": "/React-Weather-App/static/js/runtime-main.7f64f9ff.js"
+  }
+]);
